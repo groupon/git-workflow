14281415ee13156f94b7e94fda3fc8ac4e852ed9
David Bushong <david@transposit.com>
2020-10-29 11:54:02 -0700

    test: done: add test for squash merge detect

--- test/done.test.js
+++ test/done.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const assert = require('assertive');
 
 const addHooks = require('./test-common');
@@ -10,6 +12,24 @@ describe('done', () => {
   const t = addHooks();
   it('cleans up a merged feature branch', async () => {
     await startAction({ deps: t, args: ['kittens'], opts: {} });
+    await doneAction({ deps: t });
+    assert.notInclude('kittens', (await t.git.branchLocal()).all);
+  });
+
+  it('cleans up a squash merged feature branch', async () => {
+    // 1. creates a feature branch and switches to it
+    await startAction({ deps: t, args: ['kittens'], opts: {} });
+
+    // 2. simulate a squash merge with a specific diff
+    await t.git.checkout('master');
+    fs.writeFileSync('README', 'foobar\n');
+    await t.git.commit('squash merge', ['README']);
+
+    // 3. simulate the same change in the feature branch
+    await t.git.checkout('kittens');
+    fs.writeFileSync('README', 'foobar\n');
+    await t.git.commit('my change', ['README']);
+
     await doneAction({ deps: t });
     assert.notInclude('kittens', (await t.git.branchLocal()).all);
   });
